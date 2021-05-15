ef0c22ed5a8c37af6056f25346186737ff8410a6
Youssef Najjarine <youssefnajjarine@hotmail.com>
2021-05-15 11:24:07 -0700

    Listen for 'submit' events on the journal entry form and:. #6

--- js/main.js
+++ js/main.js
@@ -2,9 +2,16 @@
 /* exported data */
 const entryFormImg = document.querySelector('[data-view="entry-form"] img');
 const entryFormPhotoUrl = document.querySelector('#photoUrl');
+const entryFormForm1 = document.querySelector('.form1');
 
 entryFormPhotoUrl.addEventListener('input', handleUrl);
+entryFormForm1.addEventListener('submit', handleSaveButton);
 
 function handleUrl() {
   entryFormImg.src = entryFormPhotoUrl.value;
+}
+
+function handleSaveButton(event) {
+  event.preventDefault();
+
 }
