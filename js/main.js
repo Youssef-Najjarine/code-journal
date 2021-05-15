895226d5b2236bf2bcfd5165037b79da01a021dc
Youssef Najjarine <youssefnajjarine@hotmail.com>
2021-05-15 10:30:57 -0700

    Completed tasks #1-5

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 /* global data */
 /* exported data */
+const entryFormImg = document.querySelector('[data-view="entry-form"] img');
+const entryFormPhotoUrl = document.querySelector('#photoUrl');
+
+entryFormPhotoUrl.addEventListener('input', handleUrl);
+
+function handleUrl() {
+  entryFormImg.src = entryFormPhotoUrl.value;
+}
