e12d4ffab4b922c03ccfd5eb0fdf1a264eac5e4a
Youssef Najjarine <youssefnajjarine@hotmail.com>
2021-05-19 10:53:12 -0700

    Completed tasks #7-9.

--- js/main.js
+++ js/main.js
@@ -2,14 +2,21 @@
 /* exported data */
 
 const entryFormImg = document.querySelector('[data-view="entry-form"] img');
+const dataViewEntries = document.querySelector('[data-view="entries"]');
+const entryForm = document.querySelector('[data-view="entry-form"]');
 const entryFormForm1 = document.querySelector('.form1');
 const entryFormTitle = document.querySelector('#title');
 const entryFormPhotoUrl = document.querySelector('#photoUrl');
 const entryFormNotes = document.querySelector('#notes');
 const ul = document.querySelector('ul');
+const navBarEntries = document.querySelector('.navBarEntries');
+const h1 = document.querySelector('.entriesH1');
 
+window.addEventListener('DOMContentLoaded', populateEntries);
 entryFormPhotoUrl.addEventListener('input', handleUrl);
 entryFormForm1.addEventListener('submit', handleSaveButton);
+navBarEntries.addEventListener('click', handleNavBarEntries);
+h1.addEventListener('click', handleH1);
 
 function handleUrl() {
   entryFormImg.src = entryFormPhotoUrl.value;
@@ -17,7 +24,6 @@ function handleUrl() {
 
 function handleSaveButton(event) {
   event.preventDefault();
-
   const newObject = {};
   newObject.title = entryFormTitle.value;
   newObject.photoUrl = entryFormPhotoUrl.value;
@@ -28,13 +34,14 @@ function handleSaveButton(event) {
   entryFormForm1.reset();
   saveData();
   entryFormImg.src = 'images/placeholder-image-square.jpg';
+  dataViewEntries.classList.remove('hidden');
+  entryForm.classList.add('hidden');
+
 }
 
 function saveData() {
   localStorage.setItem('data', JSON.stringify(data));
 }
-
-window.addEventListener('DOMContentLoaded', populateEntries);
 
 function createEntries(entry) {
   const li = document.createElement('li');
@@ -55,8 +62,16 @@ function createEntries(entry) {
 }
 
 function populateEntries() {
-
   for (let i = 0; i < data.entries.length; i++) {
     ul.appendChild(createEntries(data.entries[i]));
   }
+}
+
+function handleNavBarEntries() {
+  dataViewEntries.classList.remove('hidden');
+  entryForm.classList.add('hidden');
+}
+function handleH1() {
+  dataViewEntries.classList.add('hidden');
+  entryForm.classList.remove('hidden');
 }
